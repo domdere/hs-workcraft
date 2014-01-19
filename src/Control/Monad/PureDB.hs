1d25822e27273f0274919fca414fcf5337ac1bd6
Dom De Re <domdere@domdere.com>
2014-01-19 16:25:07 +1100

    Added the free package to my dependencies, I think I will give Free Monads a try for the PureDB monad

--- src/Control/Monad/PureDB.hs
+++ src/Control/Monad/PureDB.hs
@@ -15,7 +15,6 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with Workcraft.  If not, see <http://www.gnu.org/licenses/>.
-
 -}
 
-
+module Control.Monad.PureDB where
